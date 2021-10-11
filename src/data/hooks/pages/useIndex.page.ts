d0a1f2ba34f33f66ff7b670154d73f4423d02076
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-10 22:47:23 -0300

    Added a integration with a API and completed the zip code validation

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -1,6 +1,7 @@
 import { UserShortInterface } from "data/@types/UserInterface";
 import { ValidationService } from "data/services/ValidationServices";
 import { useState, useMemo } from "react";
+import { ApiService } from "data/services/ApiService";
 
 export default function useIndex() {
     const [cep, setCep] = useState(""),
@@ -12,9 +13,37 @@ export default function useIndex() {
         [carregando, setCarregando] = useState(false),
         [diaristas, setDiaristas] = useState([] as UserShortInterface[]),
         [diaristasRestantes, setDiaristasRestantes] = useState(0);
+
+    async function buscarProfissionais(cep: string) {
+        setBuscaFeita(false);
+        setCarregando(true);
+        setErro("");
+
+        try {
+            const { data } = await ApiService.get<{
+                diaristas: UserShortInterface[];
+                quantidade_diaristas: number;
+            }>("/api/diaristas-cidade?cep=" + cep.replace(/\D/g, ""));
+            setDiaristas(data.diaristas);
+            setDiaristasRestantes(data.quantidade_diaristas);
+            setBuscaFeita(true);
+            setCarregando(false);
+        } catch (error) {
+            setErro("CEP não encontrado");
+            setCarregando(false);
+        }
+
+        setBuscaFeita(true);
+    }
     return {
         cep,
         setCep,
         cepValido,
+        buscarProfissionais,
+        erro,
+        diaristas,
+        buscaFeita,
+        carregando,
+        diaristasRestantes,
     };
 }
