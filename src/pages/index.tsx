2669e5fcc8504132188117da47a686acec6c592c
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-10 21:22:25 -0300

    Added an input for Zip code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,13 @@ import type { NextPage } from "next";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Container, Typography } from "@mui/material";
+import {
+    FormElementsContainer,
+    ProfissionaisContainer,
+    ProfissionaisPaper,
+} from "@styles/pages/index.style";
 
 const Home: NextPage = () => {
     return (
@@ -13,20 +20,64 @@ const Home: NextPage = () => {
                     "Preencha seu endereço e veja todos os profissionais da sua localidade"
                 }
             />
-
-            <UserInformation
-                name={"Felipe Vilvert Pilz"}
-                picture={"https://github.com/felipepilz.png"}
-                rating={4}
-                description={"Joinville, SC"}
-            />
-
-            <UserInformation
-                name={"Carlinhos Maia"}
-                picture={""}
-                rating={1}
-                description={"Ninguem sabe"}
-            />
+            <Container>
+                <FormElementsContainer>
+                    <TextFieldMask
+                        mask={"99.999-999"}
+                        label={"Digite seu CEP"}
+                        fullWidth
+                        variant={"outlined"}
+                    />
+                    <Typography color={"error"}>CEP Inválido</Typography>
+                    <Button
+                        variant={"contained"}
+                        color={"secondary"}
+                        sx={{ width: `220px` }}
+                    >
+                        Buscar
+                    </Button>
+                </FormElementsContainer>
+                <ProfissionaisPaper>
+                    <ProfissionaisContainer>
+                        <UserInformation
+                            name={"Felipe Vilvert Pilz"}
+                            picture={"https://github.com/felipepilz.png"}
+                            rating={4}
+                            description={"Joinville, SC"}
+                        />
+                        <UserInformation
+                            name={"Felipe Vilvert Pilz"}
+                            picture={"https://github.com/felipepilz.png"}
+                            rating={4}
+                            description={"Joinville, SC"}
+                        />
+                        <UserInformation
+                            name={"Felipe Vilvert Pilz"}
+                            picture={"https://github.com/felipepilz.png"}
+                            rating={4}
+                            description={"Joinville, SC"}
+                        />
+                        <UserInformation
+                            name={"Felipe Vilvert Pilz"}
+                            picture={"https://github.com/felipepilz.png"}
+                            rating={4}
+                            description={"Joinville, SC"}
+                        />
+                        <UserInformation
+                            name={"Felipe Vilvert Pilz"}
+                            picture={"https://github.com/felipepilz.png"}
+                            rating={4}
+                            description={"Joinville, SC"}
+                        />
+                        <UserInformation
+                            name={"Carlinhos Maia"}
+                            picture={""}
+                            rating={1}
+                            description={"Ninguem sabe"}
+                        />
+                    </ProfissionaisContainer>
+                </ProfissionaisPaper>
+            </Container>
         </div>
     );
 };
---
+++ src/ui/components/inputs/TextField/TextField.style.tsx
@@ -0,0 +1,12 @@
+import { TextField } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const TextFieldStyled = styled(TextField)`
+    .MuiInputBase-root {
+        background-color: ${({ theme }) => theme.palette.grey[50]};
+    }
+
+    .MuiOutlinedInput-notchedOutline {
+        border-color: ${({ theme }) => theme.palette.grey[100]};
+    }
+`;
