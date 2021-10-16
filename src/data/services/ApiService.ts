e40df0a8a821103fcea75bcbe3502e4a845eb02e
Felipe Pilz <felipevilvertpilz@gmail.com>
2021-10-16 01:37:31 -0300

    Added connection with API diaristas

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://ediaristas-workshop.herokuapp.com";
+const url = "http://localhost:8080";
 
 export const ApiService = axios.create({
     baseURL: url,
